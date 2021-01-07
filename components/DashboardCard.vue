5c857a091f0af8a05412ef7a060e21f1f183ec45
52339597 <hasan.almujtaba01@gmail.com>
2021-01-07 13:33:47 +0700

    change icon & other

--- components/DashboardCard.vue
+++ components/DashboardCard.vue
@@ -14,9 +14,9 @@
     <v-col cols="12" lg="4">
       <v-card flat class="tw-rounded-lg">
         <v-card-title class="text-center">
-          <v-icon color="green accent-3" class="mx-auto" x-large
-            >mdi-account-group</v-icon
-          >
+          <v-icon color="green accent-3" class="mx-auto" x-large>
+            mdi-circle
+          </v-icon>
           <br />
         </v-card-title>
         <v-card-text>
@@ -28,7 +28,7 @@
             depressed
             color="primary"
             nuxt
-            to="/admin/exhibitors"
+            to="/admin/examples"
           >
             <v-icon class="mb-1 mr-2">mdi-cog</v-icon> Configuration
           </v-btn>
@@ -39,16 +39,22 @@
     <v-col cols="12" lg="4">
       <v-card flat class="tw-rounded-lg">
         <v-card-title class="text-center">
-          <v-icon color="orange darken-4" class="mx-auto" x-large
-            >mdi-calendar-outline</v-icon
-          >
+          <v-icon color="orange darken-4" class="mx-auto" x-large>
+            mdi-circle
+          </v-icon>
           <br />
         </v-card-title>
         <v-card-text>
           <h5 class="text-h6 text-center"><b>3</b> Item</h5>
         </v-card-text>
         <v-card-actions>
-          <v-btn width="100%" depressed color="primary" nuxt to="/admin/events">
+          <v-btn
+            width="100%"
+            depressed
+            color="primary"
+            nuxt
+            to="/admin/examples"
+          >
             <v-icon class="mb-1 mr-2">mdi-cog</v-icon> Configuration
           </v-btn>
         </v-card-actions>
@@ -58,16 +64,22 @@
     <v-col cols="12" lg="4">
       <v-card flat class="tw-rounded-lg">
         <v-card-title class="text-center">
-          <v-icon color="teal darken-1" class="mx-auto" x-large
-            >mdi-file</v-icon
-          >
+          <v-icon color="teal darken-1" class="mx-auto" x-large>
+            mdi-circle
+          </v-icon>
           <br />
         </v-card-title>
         <v-card-text>
           <h5 class="text-h6 text-center"><b>3</b> Item</h5>
         </v-card-text>
         <v-card-actions>
-          <v-btn width="100%" depressed color="primary" nuxt to="/admin/pages">
+          <v-btn
+            width="100%"
+            depressed
+            color="primary"
+            nuxt
+            to="/admin/examples"
+          >
             <v-icon class="mb-1 mr-2">mdi-cog</v-icon> Configuration
           </v-btn>
         </v-card-actions>
